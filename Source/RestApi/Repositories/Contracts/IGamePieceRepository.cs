e3da47ab8a43aa5d85ca473fbb01d47034e38000
Baverstrand <70134415+Baverstrand@users.noreply.github.com>
2021-05-12 15:50:02 +0200

    IGamePieceRepository added

---
+++ Source/RestApi/Repositories/Contracts/IGamePieceRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RestApi.Models;
+
+namespace RestApi.Repositories.Contracts
+{
+    public interface IGamePieceRepository
+    {
+        bool IsCoastClear(int diceRoll, GamePlayer gamePlayer);
+        bool IsPieceInGoal(int diceRoll, GamePlayer gamePlayer);
+
+        Task Move();
+    }
+}
