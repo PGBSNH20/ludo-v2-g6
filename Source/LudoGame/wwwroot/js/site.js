588d00dff09cad75c7e7d4bb5e2c5a72c39608fc
SaAlgervik <70169634+SaAlgervik@users.noreply.github.com>
2021-05-21 14:17:46 +0200

    Update site.js

--- Source/LudoGame/wwwroot/js/site.js
+++ Source/LudoGame/wwwroot/js/site.js
@@ -10,48 +10,6 @@
 const { Button } = require("bootstrap");
 
 
-//var requestOptions = {
-//    method: "GET",
-//    headers: {
-//        'Content-Type': 'application/json',
-//    },
-//    redirect: "follow",
-//};
-
-//var response1 = await fetch("https://localhost:44369/api/Gameboard/history", requestOptions);
-//var data = await response1.json();
-//console.log(data);
-//console.log("hej")
-//return data;
-//const Url = 'https://localhost:5002/api/gamepiece?id=2ef39deb-41cf-422f-c9d3-08d91b59e9f3';
-
-//$.ajax({
-//    url: Url,
-//    type: "GET",
-//    contentType:"application/x-www-form-urlencoded; charset=utf-8",
-//    success: function (result) {
-//        console.log(result)
-//    },
-//    error: function (error) {
-//        console.log(`Error ${error}`)
-//    }
-//}),
-//    console.log("hej")
-
-
-//await fetch(url)
-//    .then(response => {
-//        const contentType = response.headers.get('content-type');
-//        if (!contentType || !contentType.includes('application/json')) {
-//            throw new TypeError("Oops, we haven't got JSON!");
-//        }
-//        return response.json();
-//    })
-//    .then(data => console.log(data))
-//    .catch(error => console.error('Unable to get items.', error));
-
-//}
-
 /*const url = 'https://localhost:44369/api/gamepiece?id=2ef39deb-41cf-422f-c9d3-08d91b59e9f3';*/
 async function getItems(id) {
     const respons = await fetch(`https://localhost:44369/api/gamepiece?id=${id}`);
