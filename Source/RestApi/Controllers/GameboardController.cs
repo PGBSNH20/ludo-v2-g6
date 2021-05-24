96d4b31765b6ca8af6e3a699075b795ec33acc5e
Joakim Widell <70150296+joakimwidell@users.noreply.github.com>
2021-05-24 11:39:26 +0200

    MErge fix

--- Source/RestApi/Controllers/GameboardController.cs
+++ Source/RestApi/Controllers/GameboardController.cs
@@ -56,7 +56,7 @@ namespace RestApi.Controllers
         public async Task<IActionResult> GetTest([FromBody]GetMoveRequest gmr)
         {
             if (!await _gamePlayerRepository.ValidateGamePlayerAsync(Guid.Parse(gmr.GamePlayerId)))
-                return BadRequest("This is not your gamepiece!");
+                return BadRequest("This is not your piece!");
             var gameBoard = await _gameBoardRepository.GetCurrentGameBoardAsync(Guid.Parse(gmr.GameBoardId));
             var gamePiece = _gamePieceRepository.GetGamePiece(gameBoard, Guid.Parse(gmr.GamePieceId));
 
