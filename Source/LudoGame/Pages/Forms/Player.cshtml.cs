9ff95bb24b819cd13bbca5d4ef5081395f7d86f5
SaAlgervik <70169634+SaAlgervik@users.noreply.github.com>
2021-05-26 11:26:05 +0200

    Update Player.cshtml.cs

--- Source/LudoGame/Pages/Forms/Player.cshtml.cs
+++ Source/LudoGame/Pages/Forms/Player.cshtml.cs
@@ -34,30 +34,21 @@ namespace LudoGame.Pages.Forms
                 var result = JsonConvert.DeserializeObject<GameBoard>(responseContent);
                 GameBoard = result;
 
-                await Task.Delay(1);
-
                 GamePlayer = result.GamePlayer.ToList();
-              
             }
             baseURL = new Uri($"https://localhost:44369/api/gamepiece?id=");
-            
 
-             response = await client.GetAsync(baseURL + id);
+            response = await client.GetAsync(baseURL + id);
 
             if (response.IsSuccessStatusCode)
             {
                 responseContent = await response.Content.ReadAsStringAsync();
 
                 var result = JsonConvert.DeserializeObject<List<GamePieceDTO>>(responseContent);
-
-                await Task.Delay(1);
-
                 GamePieces = result;
-
             }
 
             return Page();
-
         }
     }
 }
